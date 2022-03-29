e5de20f7e10c12b1fa5f27b3c973f34b8eeb2907
Fiona <fiona_berkery@hotmail.com>
2022-03-29 20:48:31 +0100

    exc q4 completed

--- exercise_c_uk.py
+++ exercise_c_uk.py
@@ -8,15 +8,22 @@ united_kingdom = [
 
 # 1. Change the capital of Wales from `"Swansea"` to `"Cardiff"`.
 united_kingdom [1][3] = "Cardiff"
-print(united_kingdom[1][3])
+# print(united_kingdom[1][3])
 
 # 2. Create a dictionary for Northern Ireland and add it to the `united_kingdom` list (The capital is Belfast, and the population is 1,811,000).
 united_kingdom.append({"name": "Northern Ireland", "population": 1811000, "capital": "Belfast"})
-print(united_kingdom)
+# print(united_kingdom)
 
 # 3. Use a loop to print the names of all the countries in the UK.
 for countries in united_kingdom:
-  print(f'{countries["name"]}')
+  # print(f'{countries["name"]}')
 
 
 # 4. Use a loop to find the total population of the UK.
+
+  total = 0
+
+for total_population in united_kingdom:
+  total += total_population["population"]
+
+print(total)
