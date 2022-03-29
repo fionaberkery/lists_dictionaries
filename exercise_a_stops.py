3f65ce7a1c6ce29d86b8b718fffa8d2d2666154c
Fiona <fiona_berkery@hotmail.com>
2022-03-29 18:57:19 +0100

    exa q7 completed

--- exercise_a_stops.py
+++ exercise_a_stops.py
@@ -17,9 +17,10 @@ stops.remove("Livingston")
 
 #6. Delete "Cumbernauld" from the list by index
 stops.pop(2)
-print(stops)
 
 #7. Print the number of stops there are in the list
+print(len(stops))
+
 #8. Sort the list alphabetically
 #9. Reverse the positions of the stops in the list
 #10 Print out all the stops using a for loop
