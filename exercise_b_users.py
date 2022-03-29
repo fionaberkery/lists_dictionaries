c5e0ae85309d117fd357230bfd0b1a715f5e76ce
Fiona <fiona_berkery@hotmail.com>
2022-03-29 19:49:15 +0100

    exb q5 completed

--- exercise_b_users.py
+++ exercise_b_users.py
@@ -70,6 +70,9 @@ print(users["Erik"]["lottery_numbers"])
 print(users["Avril"]["pets"][0]["species"])
 
 # 5. Get the smallest of Erik's lottery numbers
+smallest = min(users["Erik"]["lottery_numbers"])
+print(f"smallest number is : {smallest}")
+
 # 6. Return an list of Avril's lottery numbers that are even
 # 7. Erik is one lottery number short! Add the number `7` to be included in his lottery numbers
 # 8. Change Erik's hometown to Edinburgh
