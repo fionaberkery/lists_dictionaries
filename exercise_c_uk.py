6efcf258c68fce2649d3eee56c482eb58348373b
Fiona <fiona_berkery@hotmail.com>
2022-03-29 20:43:45 +0100

    exc q3 completed

--- exercise_c_uk.py
+++ exercise_c_uk.py
@@ -1,19 +1,9 @@
+
+
 united_kingdom = [
-  {
-    "name": "Scotland",
-    "population": 5295000,
-    "capital": "Edinburgh"
-  },
-  {
-    "name": "Wales",
-    "population": 3063000,
-    "capital": "Swansea"
-  },
-  {
-    "name": "England",
-    "population": 53010000,
-    "capital": "London"
-  }
+  {"name": "Scotland", "population": 5295000,"capital": "Edinburgh"},
+  {"name": "Wales","population": 3063000,"capital": "Swansea"},
+  {"name": "England","population": 53010000,"capital": "London"}
 ]
 
 # 1. Change the capital of Wales from `"Swansea"` to `"Cardiff"`.
@@ -25,4 +15,8 @@ united_kingdom.append({"name": "Northern Ireland", "population": 1811000, "capit
 print(united_kingdom)
 
 # 3. Use a loop to print the names of all the countries in the UK.
+for countries in united_kingdom:
+  print(f'{countries["name"]}')
+
+
 # 4. Use a loop to find the total population of the UK.
